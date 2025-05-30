7664a4bd31dee24eec968d1019c3e00251001daa
Bhavani8123 <22nn1a1202@gmail.com>
2025-05-30 15:57:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const amountInput= document.getElementById("amount");
+const fromCurrencySelect= document.getElementById("fromCurrency");
+const toCurrencySelect= document.getElementById("toCurrency");
+const convertButton= document.getElementById("convertButton");
+const resultDiv= document.getElementById("result");
+const apiUrl="https://api.exchangerate-api.com/v4/latest";
+
+convertButton.addEventListener("click",()=>{
+    const amount = parseFloat(amountInput.value);
+    const fromCurrency= fromCurrencySelect.value;
+    const toCurrency= toCurrencySelect.value;
+    
+    const query = `${apiUrl}/${fromCurrency}`;
+
+    fetch(query).then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data, "the second then block");
+
+        const exchangeRate=data.rates[toCurrency];
+
+
+
+        const convertedAmount = amount*exchangeRate;
+        resultDiv.textContent = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+            2
+        )} ${toCurrency}`;
+        //resultDiv.classList.remove("error-message");
+        //resultDiv.classList.add("success-message");
+    });
+});
